8d4b21bb47ec54e890c62ddf0558c0f0f21e43fd
PhaestusFox <phaestusfox@gmail.com>
2025-06-06 16:01:28 +0800

    added cheats that do nothing

--- src/voxels/cellular_automata/debugging.rs
+++ src/voxels/cellular_automata/debugging.rs
@@ -1,15 +1,54 @@
 use super::*;
-use bevy::prelude::*;
-
 pub fn plugin(app: &mut App) {
     app.add_systems(
         Update,
-        (melt_all, boil_all, set_all_20c).run_if(can_fuck_with_next_step),
+        (melt_all, boil_all, set_all_300k)
+            .run_if(can_fuck_with_next_step)
+            .run_if(is_cheat),
     );
 }
 
-fn melt_all(mut chunks: Query<&mut Cells>, input: Res<ButtonInput<KeyCode>>) {}
+fn melt_all(mut chunks: Query<&mut Cells>, input: Res<ButtonInput<KeyCode>>) {
+    if !input.just_pressed(KeyCode::KeyM) {
+        return;
+    }
+    for mut chunk in &mut chunks {
+        for block in chunk.iter_mut() {
+            block.flags.set(CellFlags::IS_LIQUID, true);
+        }
+    }
+}
 
-fn boil_all() {}
+fn boil_all(mut chunks: Query<&mut Cells>, input: Res<ButtonInput<KeyCode>>) {
+    if !input.just_pressed(KeyCode::KeyG) {
+        return;
+    }
+    for mut chunk in &mut chunks {
+        for block in chunk.iter_mut() {
+            block.flags.set(CellFlags::IS_GAS, true);
+        }
+    }
+}
 
-fn set_all_20c() {}
+fn set_all_300k(mut chunks: Query<&mut Cells>, input: Res<ButtonInput<KeyCode>>) {
+    if !input.just_pressed(KeyCode::KeyF) {
+        return;
+    }
+    for mut chunk in &mut chunks {
+        for block in chunk.iter_mut() {
+            let props = block.block.block_properties();
+            let le = props.heat_capacity;
+            let mp = props.mass;
+            let re = if mp < FixedNum::lit("300") {
+                le * FixedNum::lit("300") + mp
+            } else {
+                le * FixedNum::lit("300")
+            };
+            block.energy = re;
+        }
+    }
+}
+
+fn is_cheat(input: Res<ButtonInput<KeyCode>>) -> bool {
+    input.pressed(KeyCode::Backquote)
+}
